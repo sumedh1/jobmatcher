33e5bbb8e562f64373ec5b512a96cd98b338811b
sumedh1 <sumedhpatil6342@gmail.com>
2026-06-05 15:31:33 +0530

    update server.js file

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const multer = require('multer');
-const pdfParse = require('pdf-parse');
+const pdfParse = require('pdf-parse/lib/pdf-parse.js');
 const axios = require('axios');
 require('dotenv').config();
 
